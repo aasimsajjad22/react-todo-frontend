06f53ec9fdda59ae776704b867c1a0e66024611b
Aasim <aas@ciklum.com>
2021-11-30 15:08:13 +0500

    create first integration test for adding todo

--- src/e2etesting/e2e.test.js
+++ src/e2etesting/e2e.test.js
@@ -1,6 +1,6 @@
 import puppeteer from "puppeteer";
 
-const appUrlBase = 'http://localhost:3000'
+const appUrlBase = 'http://localhost:3000';
 
 let browser
 let page
@@ -10,13 +10,29 @@ beforeAll(async () => {
   page = await browser.newPage()
 })
 
-describe('contains page heading', () => {
-    test('It should display Todo App heading in h1 tag ', async () => {
-      await page.goto(`${appUrlBase}/`)
-      await page.waitForSelector('h1')
-      const result = await page.evaluate(() => {
-        return document.querySelector('h1').innerText
-      })
-      expect(result).toEqual('Todo App')
+describe('Todolist app test', () => {
+  test('It should display Todo App heading in h1 tag ', async () => {
+    await page.goto(`${appUrlBase}/`)
+    await page.waitForSelector('h1')
+    const result = await page.evaluate(() => {
+      return document.querySelector('h1').innerText
     })
+    expect(result).toEqual('Todo App')
+  })
 })
+
+describe('Should be able add buy some milk in todolist', () => {
+  test('It allow me to write text in textbox', async () => {
+    const todoText = 'buy some milk';
+    await page.goto(`${appUrlBase}/`)
+    await page.waitForSelector('h1')
+    await page.click('#form-input__todotext');
+    await page.type('#form-input__todotext', todoText);
+    
+    const text = await page.$eval("#form-input__todotext", (input) => {
+      return input.getAttribute("value")
+    });
+    expect(text).toEqual(todoText)  })
+})
+
+afterAll(() => browser.close());
