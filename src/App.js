30271403a9c5d8d0c225f24a1ad28d5d4ac56821
Aasim <aas@ciklum.com>
2021-12-01 18:27:44 +0500

    basic boilerplate for business logic is in place unit test passes for component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import AddTodo from "./components/todo/AddTodo";
+import actions from './actions/todo';
 
-const App = ({submitTodo}) => {
-
-  return (
+export const App = ({submitTodo}) => (
     <div className="App">
       <h1>Todo App</h1>
 
@@ -16,10 +16,21 @@ const App = ({submitTodo}) => {
       
       </ul>
     </div>
-  );
-}
+);
+
 App.propTypes = {
   submitTodo: PropTypes.func.isRequired,
 }
 
-export default App
+const mapStateToProps = state => state.todoListApp;
+
+const mapDispatchToProps = dispatch => ({
+  submitTodo: (text) => {
+    if (text) {
+      dispatch(actions.submitTodo(text));
+    }
+  },
+})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
-import react from "react";
+import React from "react";
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import Enzyme from 'enzyme';
-
 import { shallow } from 'enzyme'
-
+import { initialState } from './reducers/todo';
+import { Provider } from "react-redux";
+import store from './store';
 import App from "./App";
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -12,9 +13,14 @@ describe( 'App component', () => {
   
   it('Should contains AddTodo component for adding todos', () => {
     const mockFunction = jest.fn();
-
-    const wrapper = shallow(<App submitTodo={mockFunction} />);
+    const wrapper = shallow(
+      <Provider store={store}>
+        <App 
+          initialState={initialState}
+          submitTodo={mockFunction} 
+        />
+      </Provider>
+      );
     expect(wrapper.exists()).toEqual(true)
   })
-  
 });
